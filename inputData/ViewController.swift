6a0eebc74f54a905cd1dfee2d57c846441ec589f
camilo cabana <camilo_11_04@hotmail.com>
2020-08-07 10:10:29 +0900

    Initial Commit

---
+++ inputData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  inputData
+//
+//  Created by Camilo Cabana on 7/08/20.
+//  Copyright © 2020 Camilo Cabana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
